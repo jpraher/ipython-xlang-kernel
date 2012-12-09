475dfb1793b4b4103d2fdf625ce68e1ac174849b
Jakob Praher <jakob@praher.info>
2012-12-09 22:08:55 +0100

    Added C language interface (api.h).

---
+++ src/api.cpp
@@ -0,0 +1,4 @@
+
+#include "api.h"
+#include "kernel.h"
+#include "ipython_message.h"
---
+++ src/api.h
@@ -0,0 +1,80 @@
+/* -----------------------------------------------------------------------------
+ * Copyright (C) 2012 Jakob Praher
+ *
+ * Distributed under the terms of the BSD License. The full license is in
+ * the file COPYING, distributed as part of this software.
+ * -----------------------------------------------------------------------------
+ */
+
+#ifndef __ipython_shell_handler__h__
+#define __ipython_shell_handler__h__
+
+typedef
+struct ipython_message {
+    char * msg_type;
+
+    char * header;
+    char * parent;
+    char * metatdata;
+    char * content;
+} ipython_message_t;
+
+
+typedef
+struct ipython_execute_request {
+    const char * code;
+
+} ipython_execute_request_t;
+
+
+typedef
+struct ipython_execute_response {
+    // maybe directly set
+    // char * data_text_plain;
+    int  successful;
+    char * media_type;
+    char * data;
+} ipython_execute_response_t;
+
+
+
+
+typedef int (*ServiceFunction)(void * ctx,
+                               const ipython_message_t * request,
+                               ipython_message_t       * response
+                               );
+
+typedef int (*ExecuteRequestFunction)(void * ctx,
+                                      const ipython_execute_request_t * request,
+                                      ipython_execute_response_t      * response
+                                      );
+
+
+// opaque structure
+struct kernel_t;
+struct shell_handler_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+kernel_t* new_kernel_with_conection_file(const char* connection_file);
+void free_kernel(kernel_t*);
+
+int kernel_start(kernel_t*);
+int kernel_stop(kernel_t*);
+void kernel_set_service_handler(kernel_t*, shell_handler_t*);
+
+
+/**
+ * IPython shell handler
+ */
+shell_handler_t *new_ipython_shell_handler();
+void free_ipython_shell_handler(shell_handler_t*);
+void ipython_shell_handler_ctx(shell_handler_t*);
+void ipython_shell_handler_generic(shell_handler_t*,ServiceFunction);
+void ipython_shell_handler_execute_request(shell_handler_t*,ExecuteRequestFunction);
+
+#ifdef __cplusplus
+}
+#endif
